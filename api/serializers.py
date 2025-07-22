7f9552664265b570e241e78790e4cdae500c1bcf
Yogs # <mafiatamilan@yahoo.com>
2025-07-22 22:45:29 +0530

    User Creation added

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,14 @@
-from django.contrib.auth import User 
-
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
     class Meta:
         model = User
-        fields = ['username','email','password','password2']
+        fields = ['username', 'email', 'password', 'password2']
         extra_kwargs = {
-            'password' : {'write_only' : True}
+            'password': {'write_only': True}
         }
 
     def validate(self, attrs):
@@ -17,13 +17,18 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         password = attrs.get('password')
         password2 = attrs.get('password2')
 
-        if User.objects.filter(username = username).exists():
+        if User.objects.filter(username=username).exists():
             raise serializers.ValidationError({"username": "Username is already taken."})
-        
+
         if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError({"email" : "Emaiil is already registered."})
+            raise serializers.ValidationError({"email": "Email is already registered."})
 
         if password != password2:
-            raise serializers.ValidationError({"password": "Password fields didn't match"})
-        
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
         return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')  # remove password2 since it's not in the User model
+        user = User.objects.create_user(**validated_data)  # this hashes the password properly
+        return user
