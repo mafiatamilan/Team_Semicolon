f19028a79b8fc005e9f7c66b09a9b418e17bf9c1
Yogs # <mafiatamilan@yahoo.com>
2025-07-22 22:44:26 +0530

    Created end points

--- api/urls.py
+++ api/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
+from .views import UserRegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+]
