8ed7bf5103ae2474f0d3aea52837bc9f7e27c869
Yogs # <mafiatamilan@yahoo.com>
2025-07-22 22:43:41 +0530

    Updated Views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': {
+                'username': user.username,
+                'email': user.email,
+            },
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+
